7c5c3070613b72edf673a80934e45450d4ba83e3
Kurt <Kurt@10.18.151.152>
2014-06-20 10:13:27 -0600

    Added MatchingControl

--- AMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/view/MatchingGameView.java
+++ AMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/view/MatchingGameView.java
@@ -10,6 +10,38 @@ package byui.cit260.aMedievalHarvestFestival.view;
  *
  * @author Kurt
  */
-public class MatchingGameView {
+public class MatchingGameView extends MenuView {
+    public MatchingGameView() {
+    super("\n"
+            + "\n--------------------------"
+            + "\n| Matching Game           "
+            + "\nC - Choose a Match        "
+            + "\nQ - Leave the Matching Game"
+            + "\n--------------------------");
+    }
+    
+    public void doMatchingAction(String selection) {
+        switch (selection) {
+            case "00":
+             //   MatchingControl. = ;
+            break;    
+        }
+        
+    }
+
+    @Override
+    public void doAction(char value) {
+        switch(value) {
+            case 'C':
+            System.out.println("\nPlease select a location using the coodinates of"
+                             + "\nshown in matching game map.     ");
+            this.doMatchingAction(null);
+            break;
+        }
+        
+    }
+    
+            
+        
     
 }
